42598b86f7d92b33958304cfea4496e4ecdffd6c
YICHUNLIN <yichun03@gmail.com>
2025-11-20 17:42:36 +0800

    home

--- localweb/src/components/Home/index.js
+++ localweb/src/components/Home/index.js
@@ -7,21 +7,35 @@ import { useEffect } from 'react';
 import { connect } from '../../storage/context';
 import { PageContainer,PageHeaderToolbar } from '@toolpad/core/PageContainer';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+const items = [
+  {
+    version: 'Beta-0.01',
+    content: '初次上線',
+    updatedAt: '2025/11/20',
+    updatedBy: '林逸群'
+  }
+]
 const Home = ({ pathname, ...others }) => {
-  useEffect(() => {
-    console.log(others)
-  }, [others])
   return (
     <PageContainer>
       <Table>
         <TableHead>
           <TableRow>
+            <TableCell>版本</TableCell>
             <TableCell>更新日期</TableCell>
             <TableCell>更新項目</TableCell>
             <TableCell>更新者</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
+            {
+              items.map((it, i) => <TableRow key={`row_${i}`}>
+                <TableCell>{it.version}</TableCell>
+                <TableCell>{it.updatedAt}</TableCell>
+                <TableCell>{it.content}</TableCell>
+                <TableCell>{it.updatedBy}</TableCell>
+              </TableRow>)
+            }
             
         </TableBody>
       </Table>
