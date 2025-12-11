0d2e3751862dcd5d71a19c83b5b3546a8bd6bccc
YICHUNLIN <yichun03@gmail.com>
2025-12-11 19:34:39 +0800

    update version desc

--- localweb/src/components/Home/index.js
+++ localweb/src/components/Home/index.js
@@ -9,6 +9,12 @@ import { PageContainer,PageHeaderToolbar } from '@toolpad/core/PageContainer';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 const items = [
   {
+    version: 'Beta-0.1',
+    content: '進料模式',
+    updatedAt: '2025/12/12',
+    updatedBy: '林逸群'
+  },
+  {
     version: 'Beta-0.04',
     content: '取得地磅資料',
     updatedAt: '2025/12/03',
